e5c179e92fdb0cd9c82004e86dabdd49aba37566
Jared <jared.j.lorince@gmail.com>
2016-09-16 16:51:35 -0500

    trying parallel version

--- LastFM/song-info-parallel.py
+++ LastFM/song-info-parallel.py
@@ -10,12 +10,13 @@ import sys
 
 
 API_KEY,API_SECRET = open('lastfm.apikey').readlines()
-outfile = 'data/lastfm_top_similar_artists_new'
-datadir = 'P:/Projects/BigMusic/jared.rawdata/item_data/'
+datadir = 'item_data/'
+itemlist_dir = 'lastfm_itemlist.txt'
+#itemlist_dir = 'P:/Projects/BigMusic/jared.rawdata/lastfm_itemlist.txt'
 
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
 
-item_data = pd.read_table('P:/Projects/BigMusic/jared.rawdata/lastfm_itemlist.txt',header=None,names=['item_id','item_type','artist','artist_id','album','song','item_url','top_tag','total_scrobbles','unique_listeners'],nrows=10000)#.sort_values(by='total_scrobbles',ascending=False)
+item_data = pd.read_table(itemlist_dir,header=None,names=['item_id','item_type','artist','artist_id','album','song','item_url','top_tag','total_scrobbles','unique_listeners'],nrows=10000)#.sort_values(by='total_scrobbles',ascending=False)
 
 item_data = item_data[item_data['item_type']!=1][['item_id','item_type','artist','song']]
 
